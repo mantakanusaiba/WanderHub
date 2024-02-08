448fb51c26d0be5e38dd574282562219454a1e02
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2024-02-08 08:06:16 +0600

    Update roomselectionpage.dart

--- lib/roomselectionpage.dart
+++ lib/roomselectionpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class RoomSelectionPage extends StatelessWidget {
   final Map<String, dynamic> hotel;
 
@@ -12,49 +11,98 @@ class RoomSelectionPage extends StatelessWidget {
         title: Text('Room Selection'),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Card 1 - Selected Hotel
-              buildHotelCard({
-                'name': 'Deluxe',
-                'price': 1500,
-                'image':
-                'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/10/b4/05/the-cox-today-limited.jpg?w=300&h=200&s=1',
-              }),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildHotelCard(context, {
+                  'name': 'Deluxe Room',
+                  'price': 5200,
+                  'image':
+                  'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/10/b4/05/the-cox-today-limited.jpg?w=300&h=200&s=1',
+                  'facilities': [
+                    {'icon': Icons.hotel, 'name': 'Extra Bed'},
+                    {'icon': Icons.iron, 'name': 'Iron Facilities'},
+                    {'icon': Icons.local_drink, 'name': 'Bottled Water'},
+                    {'icon': Icons.wifi, 'name': 'Wi-Fi'},
+                    {'icon': Icons.bathtub, 'name': 'Shower'},
+                    {'icon': Icons.tv, 'name': 'TV'},
+                    {'icon': Icons.free_breakfast, 'name': 'Tea'},
+                    {'icon': Icons.local_cafe, 'name': 'Coffee Maker'},
+                    {'icon': Icons.ac_unit, 'name': 'Air Conditioning'},
+                    {'icon': Icons.kitchen, 'name': 'Fridge'},
+                    {'icon': Icons.bathroom, 'name': 'Personal Bathroom'},
+                  ],
+                }),
+                buildHotelCard(context, {
+                  'name': 'Deluxe Twin Room',
+                  'price': 4460,
+                  'image':
+                  'https://www.momondo.com/himg/3c/7c/4b/expediav2-713372-d206e5-950457.jpg',
+                  'facilities': [
+                    {'icon': Icons.wifi, 'name': 'Wi-Fi'},
+                    {'icon': Icons.iron, 'name': 'Iron Facilities'},
+                    {'icon': Icons.bathtub, 'name': 'Shower'},
+                    {'icon': Icons.tv, 'name': 'TV'},
+                    {'icon': Icons.free_breakfast, 'name': 'Tea'},
+                    {'icon': Icons.ac_unit, 'name': 'Air Conditioning'},
+                    {'icon': Icons.kitchen, 'name': 'Fridge'},
+                    {'icon': Icons.bathroom, 'name': 'Personal Bathroom'},
+                  ],
+                }),
 
-              // Card 2 - Example Hotel 2
-              buildHotelCard({
-                'name': 'Deluxe Twin',
-                'price': 1500,
-                'image':
-                'https://www.momondo.com/himg/3c/7c/4b/expediav2-713372-d206e5-950457.jpg',
-              }),
+                buildHotelCard(context, {
+                  'name': 'Superior Double Room',
+                  'price': 4100,
+                  'image':
+                  'https://q-xx.bstatic.com/xdata/images/hotel/max500/243701987.jpg?k=c7c1fe6c4010c987a3564aab5496e60437e107bbd4c20d04345d4651f1c7276c&o=',
+                  'facilities': [
 
-              // Card 3 - Example Hotel 3
-              buildHotelCard({
-                'name': 'Superior',
-                'price': 2000,
-                'image':
-                'https://q-xx.bstatic.com/xdata/images/hotel/max500/243701987.jpg?k=c7c1fe6c4010c987a3564aab5496e60437e107bbd4c20d04345d4651f1c7276c&o=',
-              }),
+                    {'icon': Icons.ac_unit, 'name': 'Air Conditioning'},
 
-              // Card 4 - Example Hotel 4
-              buildHotelCard({
-                'name': 'Family',
-                'price': 1800,
-                'image':
-                'https://www.momondo.com/himg/3c/7c/4b/expediav2-713372-d206e5-950457.jpg',
-              }),
-            ],
+                    {'icon': Icons.wifi, 'name': 'Wi-Fi'},
+                    {'icon': Icons.bathtub, 'name': 'Shower'},
+                    {'icon': Icons.tv, 'name': 'TV'},
+                    {'icon': Icons.free_breakfast, 'name': 'Tea'},
+                    {'icon': Icons.local_cafe, 'name': 'Coffee Maker'},
+
+                    {'icon': Icons.kitchen, 'name': 'Fridge'},
+
+                  ],
+                }),
+
+
+                buildHotelCard(context, {
+                  'name': 'Family Room',
+                  'price': 5300,
+                  'image':
+                  'https://www.momondo.com/himg/3c/7c/4b/expediav2-713372-d206e5-950457.jpg',
+                  'facilities': [
+                    {'icon': Icons.hotel, 'name': 'Extra Bed'},
+                    {'icon': Icons.iron, 'name': 'Iron Facilities'},
+
+                    {'icon': Icons.wifi, 'name': 'Wi-Fi'},
+                    {'icon': Icons.bathtub, 'name': 'Shower'},
+                    {'icon': Icons.tv, 'name': 'TV'},
+                    {'icon': Icons.free_breakfast, 'name': 'Tea'},
+
+                    {'icon': Icons.ac_unit, 'name': 'Air Conditioning'},
+                    {'icon': Icons.kitchen, 'name': 'Fridge'},
+
+
+                  ],
+                }),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget buildHotelCard(Map<String, dynamic> hotelData) {
+  Widget buildHotelCard(BuildContext context, Map<String, dynamic> hotelData) {
     return Card(
       elevation: 4.0,
       shape: RoundedRectangleBorder(
@@ -62,22 +110,22 @@ class RoomSelectionPage extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child: Image.network(
-                    hotelData['image'],
-                    width: 260,
-                    height: 150,
-                    fit: BoxFit.cover,
-                  ),
+            Flexible(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image.network(
+                  hotelData['image'],
+                  fit: BoxFit.cover,
                 ),
-                Column(
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
@@ -92,41 +140,52 @@ class RoomSelectionPage extends StatelessWidget {
                       'Price: \$${hotelData['price']}',
                       style: TextStyle(fontSize: 16),
                     ),
+                    SizedBox(height: 16),
+                    FacilityRow(
+                      hotelData['facilities']
+                          .take(3)
+                          .map<Widget>((facility) => FacilityIconButton(
+                          facility['icon'], facility['name']))
+                          .toList(),
+                    ),
+                    SizedBox(height: 8),
+                    FacilityRow(
+                      hotelData['facilities']
+                          .skip(3)
+                          .take(3)
+                          .map<Widget>((facility) => FacilityIconButton(
+                          facility['icon'], facility['name']))
+                          .toList(),
+                    ),
+                    SizedBox(height: 8),
+                    FacilityRow(
+                      hotelData['facilities']
+                          .skip(6)
+                          .take(3)
+                          .map<Widget>((facility) => FacilityIconButton(
+                          facility['icon'], facility['name']))
+                          .toList(),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Navigate to the payment page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PaymentPage(hotelData),
+                            ),
+                          );
+                        },
+                        child: Text('Book Now'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue,
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                        ),
+                      ),
+                    ),
                   ],
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            // Icons for Facilities
-            FacilityRow([
-              FacilityIconButton(Icons.hotel, 'Extra Bed'),
-              FacilityIconButton(Icons.iron, 'Iron Facilities'),
-              FacilityIconButton(Icons.local_drink, 'Bottled Water'),
-            ]),
-            FacilityRow([
-              FacilityIconButton(Icons.wifi, 'Wi-Fi'),
-              FacilityIconButton(Icons.bathtub, 'Shower'),
-              FacilityIconButton(Icons.tv, 'TV'),
-            ]),
-            FacilityRow([
-              FacilityIconButton(Icons.free_breakfast, 'Tea'),
-              FacilityIconButton(Icons.local_cafe, 'Coffee Maker'),
-              FacilityIconButton(Icons.ac_unit, 'Air Conditioning'),
-            ]),
-            FacilityRow([
-              FacilityIconButton(Icons.kitchen, 'Fridge'),
-              FacilityIconButton(Icons.bathroom, 'Personal Bathroom'),
-            ]),
-            SizedBox(height: 16),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Add your logic for the "Book Now" button
-                },
-                child: Text('Book Now'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  padding: EdgeInsets.symmetric(horizontal: 40),
                 ),
               ),
             ),
@@ -144,9 +203,11 @@ class FacilityRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> displayedChildren = children.take(3).toList();
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: children,
+      children: displayedChildren,
     );
   }
 }
@@ -160,9 +221,7 @@ class FacilityIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-
-      },
+      onPressed: () {},
       icon: Column(
         children: [
           Icon(icon),
@@ -172,6 +231,186 @@ class FacilityIconButton extends StatelessWidget {
       ),
       color: Colors.blueGrey,
       tooltip: facility,
+    );
+  }
+}
+
+
+class PaymentPage extends StatefulWidget {
+  final Map<String, dynamic> hotelData;
+
+  PaymentPage(this.hotelData);
+
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  TextEditingController cardNumberController = TextEditingController();
+  TextEditingController expirationDateController = TextEditingController();
+  TextEditingController cvvController = TextEditingController();
+  TextEditingController mobileNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String selectedPaymentMethod = 'Debit Card';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Payment for ${widget.hotelData['name']}',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Total Price: \$${widget.hotelData['price']}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Radio(
+                  value: 'Debit Card',
+                  groupValue: selectedPaymentMethod,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedPaymentMethod = value.toString();
+                    });
+                  },
+                ),
+                Text('Debit Card'),
+                Radio(
+                  value: 'BKash',
+                  groupValue: selectedPaymentMethod,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedPaymentMethod = value.toString();
+                    });
+                  },
+                ),
+                Text('BKash'),
+                Radio(
+                  value: 'Nagad',
+                  groupValue: selectedPaymentMethod,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedPaymentMethod = value.toString();
+                    });
+                  },
+                ),
+                Text('Nagad'),
+              ],
+            ),
+            if (selectedPaymentMethod == 'Debit Card')
+              SizedBox(height: 16),
+            if (selectedPaymentMethod == 'Debit Card')
+              TextFormField(
+                controller: cardNumberController,
+                decoration: InputDecoration(labelText: 'Card Number'),
+                keyboardType: TextInputType.number,
+              ),
+            if (selectedPaymentMethod == 'Debit Card')
+              SizedBox(height: 12),
+            if (selectedPaymentMethod == 'Debit Card')
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: expirationDateController,
+                      decoration: InputDecoration(labelText: 'Expiration Date'),
+                    ),
+                  ),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: TextFormField(
+                      controller: cvvController,
+                      decoration: InputDecoration(labelText: 'CVV'),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ],
+              ),
+            if (selectedPaymentMethod == 'BKash' || selectedPaymentMethod == 'Nagad')
+              SizedBox(height: 16),
+            if (selectedPaymentMethod == 'BKash' || selectedPaymentMethod == 'Nagad')
+              TextFormField(
+                controller: mobileNumberController,
+                decoration: InputDecoration(labelText: 'Mobile Number'),
+                keyboardType: TextInputType.phone,
+              ),
+            if (selectedPaymentMethod == 'BKash' || selectedPaymentMethod == 'Nagad')
+              SizedBox(height: 12),
+            if (selectedPaymentMethod == 'BKash' || selectedPaymentMethod == 'Nagad')
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+
+                handlePayment();
+              },
+              child: Text('Complete Payment'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blueGrey,
+                padding: EdgeInsets.symmetric(horizontal: 40),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void handlePayment() {
+
+    if (selectedPaymentMethod == 'Debit Card') {
+
+    } else if (selectedPaymentMethod == 'BKash') {
+
+      String mobileNumber = mobileNumberController.text;
+      String password = passwordController.text;
+
+    } else if (selectedPaymentMethod == 'Nagad') {
+
+      String mobileNumber = mobileNumberController.text;
+      String password = passwordController.text;
+
+    }
+
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Payment Successful'),
+          content: Text('Thank you for your payment!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Navigate back to the room selection page
+                Navigator.popUntil(
+                  context,
+                  ModalRoute.withName('/'),
+                );
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
